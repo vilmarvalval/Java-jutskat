3666da33d3821afb750a64da7b228123a5b81cb3
vilmarvalval <ville.valtasalo@metropolia.fi>
2026-03-17 17:45:34 +0200

    Task 2 done

---
+++ src/Task02.java
@@ -0,0 +1,45 @@
+/*
+    *
+   ***
+  *****
+ *******
+ */
+
+import java.util.Scanner;
+
+public class Task02 {
+
+    public static boolean isInt(String x){
+     try {
+         Integer.parseInt(x);
+         return true;
+     } catch (NumberFormatException ex){
+         return false;
+     }
+    }
+
+    public static void main(String[] args){
+        int amountN=0;
+        while (amountN<=0){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter tree size: ");
+            String amount = input.nextLine();
+
+            if (isInt(amount)) {
+                amountN = Integer.parseInt(amount);
+                if (amountN <=0){
+                    System.out.println("Tree size must be a number and greater than 0.");
+                }
+            } else {
+                System.out.println("Tree size must be a number and greater than 0.");
+            }
+        }
+
+        //System.out.println("Tree size will be "+ amountN);
+        for (int i=0; i<=amountN; i++){
+            String spaces = new String(new char[(amountN-i)]).replace("\0", " ");
+            String stars = new String(new char[i*2+1]).replace("\0", "*");
+            System.out.println(spaces+stars);
+        }
+    }
+}
